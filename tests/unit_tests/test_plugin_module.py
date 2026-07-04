8715995971a603ad3076f43b3ad8a6702d6c2cfd
Kyle Oliver <oliverke@mail.uc.edu>
2026-07-04 11:25:17 -0400

    test: add plugin module fixture smoke tests

---
+++ tests/unit_tests/test_plugin_module.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+import pytest
+
+from pytest_repo_structure.plugin import acceptance_tests_folder
+from pytest_repo_structure.plugin import config_folder
+from pytest_repo_structure.plugin import data_folder
+from pytest_repo_structure.plugin import integration_tests_folder
+from pytest_repo_structure.plugin import plugin_tests_folder
+from pytest_repo_structure.plugin import repository_root
+from pytest_repo_structure.plugin import tests_folder
+from pytest_repo_structure.plugin import unit_tests_folder
+
+
+@pytest.mark.parametrize(
+    argnames="fixture",
+    argvalues=[
+        repository_root,
+        tests_folder,
+        acceptance_tests_folder,
+        config_folder,
+        data_folder,
+        integration_tests_folder,
+        plugin_tests_folder,
+        repository_root,
+        tests_folder,
+        unit_tests_folder,
+    ],
+)
+def test_plugin_module_fixtures(fixture: Any) -> None:
+    assert fixture is not None
